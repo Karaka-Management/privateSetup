483670b54019a1c97bfbd1705add20bde61385bf
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-03-29 17:04:52 +0200

    continue workflow demo setup drafting

---
+++ cms/Demo/Admin/Status.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Karaka
+ *
+ * PHP Version 8.0
+ *
+ * @package   Web\{APPNAME}\Admin
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://karaka.app
+ */
+declare(strict_types=1);
+
+namespace Web\{APPNAME}\Admin;
+
+use phpOMS\Application\StatusAbstract;
+
+/**
+ * Status class.
+ *
+ * @package Web\{APPNAME}\Admin
+ * @license OMS License 1.0
+ * @link    https://karaka.app
+ * @since   1.0.0
+ */
+final class Status extends StatusAbstract
+{
+    /**
+     * Path of the file
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    public const PATH = __DIR__;
+}
